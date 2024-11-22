b62d943327820793f81e8fc117dd759067695d20
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2024-11-23 00:53:45 +0600

    Code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'UI/home.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const FurnitureApp());
 }
 
+class FurnitureApp extends StatelessWidget {
+  const FurnitureApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 121, 147, 174),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)),
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Color.fromARGB(255, 121, 147, 174),
+          size: 41,
+
+        ),
+      ),
+      title: 'Furniture App',
+      home: const Home(),
+    );
+  }
+}
